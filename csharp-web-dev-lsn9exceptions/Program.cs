ae18f790bd8e5727d3316d0c730fe49935fbbd4a
Allie Towe <towe.allie@yahoo.com>
2021-01-06 14:04:17 -0600

    final

--- csharp-web-dev-lsn9exceptions/Program.cs
+++ csharp-web-dev-lsn9exceptions/Program.cs
@@ -5,20 +5,49 @@ namespace csharp_web_dev_lsn9exceptions
 {
     class Program
     {
+
         static double Divide(double x, double y)
         {
-            // Write your code here!
+            if (y == 0)
+                throw new DivideByZeroException();
+            return x / y;
         }
 
         static int CheckFileExtension(string fileName)
         {
-            // Write your code here!
+            int point = 0;
+            if (fileName.Contains(".cs"))
+            {
+                point = 1;
+                return point;
+            }
+            else if (fileName == null | fileName == "")
+            {
+                throw new ArgumentNullException();
+
+            } else if (!fileName.Contains(".cs"))
+            {
+                point = 0;
+                return point;
+            } 
+            return point;
         }
 
 
         static void Main(string[] args)
         {
-            // Test out your Divide() function here!
+            double a = 4, b = 0;
+            double result;
+
+            try
+            {
+                result = Divide(a, b);
+                Console.WriteLine("%{a} divided by {b} equals {result}");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("You cannot divide by 0");
+            }
 
             // Test out your CheckFileExtension() function here!
             Dictionary<string, string> students = new Dictionary<string, string>();
@@ -26,6 +55,19 @@ namespace csharp_web_dev_lsn9exceptions
             students.Add("Brad", "");
             students.Add("Elizabeth", "MyCode.cs");
             students.Add("Stefanie", "CoolProgram.cs");
+
+            foreach (var kvp in students)
+            {
+                try
+                {
+                    Console.WriteLine(CheckFileExtension(kvp.Value));
+                }
+                catch (ArgumentNullException)
+                {
+                    Console.WriteLine("Cannot be null!");
+                }
+
+            }
 
 
         }
